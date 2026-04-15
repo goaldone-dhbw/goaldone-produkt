09a2ea5ab906020473f6a3f9c3196a70e80815e0
Joinsider <johannes@fampopprol.me>
2026-04-15 20:14:26 +0200

    chore: update CI configuration and improve test environment setup

--- frontend/src/test-setup.ts
+++ frontend/src/test-setup.ts
@@ -1,12 +1,14 @@
 import 'zone.js';
 import 'zone.js/testing';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
-TestBed.initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
+if (!getTestBed().platform) {
+  TestBed.initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
+  );
+}
