087169010550326296477ac15f3ae423ffd4a33d
salma <154587119+saleyy@users.noreply.github.com>
2026-04-21 11:35:11 +0200

    feat: add super-admins page with routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TestPageComponent } from './features/test-page/test-page.component';
 import { authGuard } from './core/auth/auth.guard';
 import { StartPageComponent } from './features/startpage/start-page.component';
 import { CallbackPageComponent } from './features/callback/callback-page.component';
+import { SuperAdminsPageComponent } from './features/super-admins/super-admins-page.component';
 
 export const routes: Routes = [
   {
@@ -13,6 +14,10 @@ export const routes: Routes = [
     path: 'test',
     component: TestPageComponent,
     canActivate: [authGuard],
+  },
+  {
+    path: 'admin/super-admins',
+    component: SuperAdminsPageComponent
   },
   {
     path: '',
