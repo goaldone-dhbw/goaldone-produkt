2696b7f4ed07c0276dfb2bd924f29efd157677e2
LeonProgrammiert <leon.freudenmann@gmail.com>
2026-04-22 11:22:56 +0200

    Post-merge fixing 2

--- backend/src/main/java/de/goaldone/backend/scheduler/types/model/PlanningResult.java
+++ backend/src/main/java/de/goaldone/backend/scheduler/types/model/PlanningResult.java
@@ -1,12 +1,13 @@
 package de.goaldone.backend.scheduler.types.model;
 
+import de.goaldone.backend.model.ScheduleEntry;
 import de.goaldone.backend.model.ScheduleWarning;
 
 import java.util.ArrayList;
 
 public record PlanningResult(
     int score,
-    ArrayList<ScheduledChunk> scheduledChunks,
+    ArrayList<ScheduleEntry> scheduledChunks,
     ArrayList<UnscheduledTaskResult> unscheduledTasks,
     ArrayList<ScheduleWarning> scheduleWarnings
 ) {
